c4e5433a8b45976e42ea37778331ae097547fb67
cvand <vandera.chryssanthi@gmail.com>
2015-02-26 03:39:55 -0500

    Revert de5e8ae..9eca8a4
    This rolls back to commit de5e8aedb1671a02901c58de5644080c8a98cd73.

---
+++ AlarmFit/StatisticsViewController.h
@@ -0,0 +1,16 @@
+//
+//  StatisticsViewController.h
+//  AlarmFit
+//
+//  Created by Archana Iyer on 2/25/15.
+//  Copyright (c) 2015 chrysanthicodes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface StatisticsViewController : UIViewController
+@property (strong, nonatomic) IBOutlet UILabel *sleepQuality;
+@property (strong, nonatomic) IBOutlet UILabel *sleepDuration;
+@property (strong, nonatomic) IBOutlet UILabel *sleepCycle;
+
+@end
